2a92ebcebcf1392b5087b3bece2244a22891d78d
FreeWind94 <shukin-nikita@mail.ru>
2020-02-13 19:15:02 +0300

    Добавлен MyStack

---
+++ MyCollection/MyStack.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollection
+{
+    class MyStack<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Узел стека.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class Node<T>
+        {
+            public T Data { get; set; }
+            /// <summary>
+            /// Ссылка на следующий узел
+            /// </summary>
+            public Node<T> Next { get; set; }
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+
+            public override string ToString()
+            {
+                return Data.ToString();
+            }
+        }
+
+        
+        //Peek
+        public MyStack()
+        {
+            firstNode = null;
+            Count = 0;
+        }
+
+        /*  В стек реализуем припомоши односвязного списка 
+         *  (в теории можно было бы прис от него отнаследовать 
+         *  или сделать общий класс, но я не уверен что точно 
+         *  знаю как это реализовать). Чтобы реализовать функции стека припомоши 
+         *  списка, достаточно ссылки на первый узел и счётчика количества узлов.
+         */
+        private Node<T> firstNode;
+        public int Count { get; private set; }
+
+        public bool IsEmpty { get => Count == 0; }
+
+        /// <summary>
+        /// Добавить данные в стек
+        /// </summary>
+        /// <param name="Data"></param>
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = firstNode;
+            firstNode = newNode;
+            Count++;
+        }
+
+        /// <summary>
+        /// Достать данные из стека
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            // нельзя ничего достать из пустого стека
+            if (IsEmpty) throw new InvalidOperationException("Стек пустой");
+
+            Node<T> result = firstNode;
+            firstNode = firstNode.Next;
+            Count--;
+            return result.Data;
+        }
+
+        /// <summary>
+        /// Показать данные стека не доставая их (мои комментарии божественны)
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            // нельзя показать того чего нет
+            if (IsEmpty) throw new InvalidOperationException("Стек пустой");
+
+            return firstNode.Data;
+        }
+
+        //
+        // реализация интерфейса IEnumerable<T>
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> currentNode = firstNode;
+            while (currentNode != null)
+            {
+                yield return currentNode.Data;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
--- MyCollection/Program.cs
+++ MyCollection/Program.cs
@@ -11,46 +11,59 @@ namespace MyCollection
             {
                 Console.WriteLine("Hello World!");
 
-                // ээээкспиременты!!!
-                MyList<Person> people = new MyList<Person>();
-                people.Add(new Person("Maxim", "Shchukin"));
-                people.Add(new Person("Nikita", "Shchukin"));
-                people.Add(new Person("Vasiliy", "Pupkin"));
-                people.Add(new Person("Aleshka", "Grabovod"));
-                // тест ToString
-                Console.WriteLine(people.ToString());
-                // тест foreach
-                foreach (var person in people)
-                {
-                    Console.WriteLine("Hello " + person.FirstName + "!");
-                }
-                /*
-                // тест Remove
-                Console.WriteLine("__________Test 1__________");
-                people.Remove(new Person("Vasiliy", "Pupkin"));
-                Console.WriteLine(people.ToString());
-                Console.WriteLine("__________Test 2__________");
-                people.Remove(new Person("Maxim", "Shchukin"));
-                Console.WriteLine(people.ToString());
-                Console.WriteLine("__________Test 3__________");
-                people.Remove(new Person("Aleshka", "Grabovod"));
-                Console.WriteLine(people.ToString());
-                Console.WriteLine("__________Test 4__________");
-                people.Remove(new Person("Nikita", "Shchukin"));
-                Console.WriteLine(people.ToString());
-                Console.WriteLine("Count is: " + people.Count);
-                */
+                // ээээкспиременты!!!               
+                //MyList<Person> people = new MyList<Person>();
+                //people.Add(new Person("Maxim", "Shchukin"));
+                //people.Add(new Person("Nikita", "Shchukin"));
+                //people.Add(new Person("Vasiliy", "Pupkin"));
+                //people.Add(new Person("Aleshka", "Grabovod"));
+                //// тест ToString
+                //Console.WriteLine(people.ToString());
+                //// тест foreach
+                //foreach (var person in people)
+                //{
+                //    Console.WriteLine("Hello " + person.FirstName + "!");
+                //}
+                ///*
+                //// тест Remove
+                //Console.WriteLine("__________Test 1__________");
+                //people.Remove(new Person("Vasiliy", "Pupkin"));
+                //Console.WriteLine(people.ToString());
+                //Console.WriteLine("__________Test 2__________");
+                //people.Remove(new Person("Maxim", "Shchukin"));
+                //Console.WriteLine(people.ToString());
+                //Console.WriteLine("__________Test 3__________");
+                //people.Remove(new Person("Aleshka", "Grabovod"));
+                //Console.WriteLine(people.ToString());
+                //Console.WriteLine("__________Test 4__________");
+                //people.Remove(new Person("Nikita", "Shchukin"));
+                //Console.WriteLine(people.ToString());
+                //Console.WriteLine("Count is: " + people.Count);
+                //*/
 
-                // тест Index
-                Console.WriteLine("__________Test 1__________");
-                Console.WriteLine(people.IndexOf(new Person("Vasiliy", "Pupkin")));
-                Console.WriteLine("__________Test 2__________");
-                Console.WriteLine(people[1]);
-                Console.WriteLine("__________Test 3__________");
-                people[2] = new Person("Ivan","Ivanov");
-                Console.WriteLine(people);
+                //// тест Index
+                //Console.WriteLine("__________Test 1__________");
+                //Console.WriteLine(people.IndexOf(new Person("Vasiliy", "Pupkin")));
+                //Console.WriteLine("__________Test 2__________");
+                //Console.WriteLine(people[1]);
+                //Console.WriteLine("__________Test 3__________");
+                //people[2] = new Person("Ivan", "Ivanov");
+                //Console.WriteLine(people);
+
+                MyStack<Person> people = new MyStack<Person>();
+                people.Push(new Person("Maxim", "Shchukin"));
+                people.Push(new Person("Nikita", "Shchukin"));
+                people.Push(new Person("Vasiliy", "Pupkin"));
+                people.Push(new Person("Aleshka", "Grabovod"));
+                Console.WriteLine("Count: " + people.Count);
+                Console.WriteLine("Pop: " + people.Pop());
+                Console.WriteLine("Count: " + people.Count);
+                Console.WriteLine("Peek: " + people.Peek());
+                Console.WriteLine("Count: " + people.Count);
+                Console.WriteLine("Peek: " + people.Peek());
+                Console.WriteLine("Count: " + people.Count);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
